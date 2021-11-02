f2bec80a1bfcb52cd4be11120df8814c9b9afec2
Charly <asdsad@sdsad.es>
2021-11-02 09:40:37 +0100

    Inicializado ejercicio JS
    - Creado js. REalizados ejercicios para alumnos

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+"use strcit";
+ 
+// alert(); para comprobar que el java está bien enlazado
+
+// Ejercicio 1: Crea una variable en donde se muestre tu nombre a través de la función console.log
+
+// Variables
+
+//  let miNombre = "Xena";
+
+//  console.log(miNombre); 
+
+// Ejercicio 2: Cambiar el valor de dicha variable y comprobar que efectivamente cambió
+
+// let miNombre = "Xena";
+
+// miNombre= "Gabriel"; 
+
+// console.log(miNombre);
+
+//Ejercicio 3: Comenta la variable y traba de verla con console.log() y razona lo que pasa
+// Me dice que si comento vartiable que no está definida
+
+//Ejercicio 4: Crea tres variables: operador1, operador2, resultado. Inicializa las variables. Suma operador1 y operador2 y guarda el resultado. Muestra por consola el resultado de dicha operación.
+
+// let operador1=3, operador2=5;
+
+// let resultado=0;
+
+// resultado = operador1+operador2;
+
+// console.log(resultado);
